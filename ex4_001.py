9f56b6ab7d998e52d1bce65bbccd4e2cf5c8c6cc
gutomjacques <guto.mjacques@gmail.com>
2025-02-25 21:09:12 -0300

    Add files via upload

---
+++ ex4_001.py
@@ -0,0 +1,25 @@
+"""
+Programa Oi
+Descrição: pergunta o nome do usuário e apresenta a mensagem ”Oi,...”, seguida pelo nome do usuário.
+Autor: Augusto Jacques
+Data: 25/02/2025
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+nome = ""
+frase = ""
+
+# Entrada de dados
+
+nome = str(input("\nQual o seu nome?"))
+
+# Processamento de dados
+
+frase = f"Oi, {nome}!"
+
+# Saída de dados
+
+print(frase)
+
